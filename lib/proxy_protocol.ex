44ee8eac6907cc00f08067620c82bad58971ab06
Jake Morrison <jake@cogini.com>
2024-02-24 17:51:51 -0600

    Add code

--- lib/proxy_protocol.ex
+++ lib/proxy_protocol.ex
@@ -1,18 +1,93 @@
 defmodule ProxyProtocol do
   @moduledoc """
-  Documentation for `ProxyProtocol`.
+    https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ProxyProtocol.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  @doc "Create text header"
+  @spec format_header_v1(
+          :inet.ip_address(),
+          :inet.port_number(),
+          :inet.ip_address(),
+          :inet.port_number()
+        ) :: iodata()
+  def format_header_v1(src_addr, src_port, dst_addr, dst_port) do
+    [
+      "PROXY ",
+      format_family_v1(src_addr),
+      0x20,
+      :inet.ntoa(src_addr),
+      0x20,
+      :inet.ntoa(dst_addr),
+      0x20,
+      Integer.to_string(src_port),
+      0x20,
+      Integer.to_string(dst_port),
+      0x0D,
+      0x0A
+    ]
   end
+
+  @doc "Create header family string based on type of address"
+  @spec format_family_v1(:inet.ip_address()) :: binary()
+  def format_family_v1({_, _, _, _}), do: "TCP4"
+  def format_family_v1({_, _, _, _, _, _, _, _}), do: "TCP6"
+
+  @doc "Create binary header"
+  @spec format_header_v2(
+          :inet.ip_address(),
+          :inet.port_number(),
+          :inet.ip_address(),
+          :inet.port_number()
+        ) :: iodata()
+  def format_header_v2(src_addr, src_port, dst_addr, dst_port) do
+    # proxy protocol version
+    proxy_protocol_version = 2
+    # PROXY
+    proxy_protocol_command = 1
+
+    address_family = format_family_v2(src_addr)
+    # STREAM
+    transport_protocol = 1
+
+    [
+      # signature
+      0x0D,
+      0x0A,
+      0x0D,
+      0x0A,
+      0x00,
+      0x0D,
+      0x0A,
+      0x51,
+      0x55,
+      0x49,
+      0x54,
+      0x0A,
+      <<proxy_protocol_version::4, proxy_protocol_command::4>>,
+      <<address_family::4, transport_protocol::4>>,
+      format_length_v2(address_family),
+      format_addr_v2(src_addr),
+      format_addr_v2(dst_addr),
+      format_port_v2(src_port),
+      format_port_v2(dst_port)
+    ]
+  end
+
+  @doc "Create header family based on type of address"
+  @spec format_family_v2(:inet.ip_address()) :: integer()
+  def format_family_v2({_, _, _, _}), do: 1
+  def format_family_v2({_, _, _, _, _, _, _, _}), do: 2
+
+  def format_addr_v2({b1, b2, b3, b4}) do
+    <<b1::8, b2::8, b3::8, b4::8>>
+  end
+
+  def format_addr_v2({b1, b2, b3, b4, b5, b6, b7, b8}) do
+    <<b1::8, b2::8, b3::8, b4::8, b5::8, b6::8, b7::8, b8::8>>
+  end
+
+  def format_port_v2(port), do: <<port::big-integer-size(16)>>
+
+  def format_length_v2(1), do: <<12::big-integer-size(16)>>
+  def format_length_v2(2), do: <<36::big-integer-size(16)>>
 end
--- test/proxy_protocol_test.exs
+++ test/proxy_protocol_test.exs
@@ -1,8 +1,59 @@
 defmodule ProxyProtocolTest do
-  use ExUnit.Case
-  doctest ProxyProtocol
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert ProxyProtocol.hello() == :world
+  test "format_family_v1/1" do
+    assert ProxyProtocol.format_family_v1({192, 168, 1, 1}) == "TCP4"
+  end
+
+  describe "format_header_v1/4" do
+    test "Handles IPv4" do
+      src_addr = {192, 168, 1, 1}
+      src_port = 10_000
+      dst_addr = {10, 0, 0, 1}
+      dst_port = 443
+      header = ProxyProtocol.format_header_v1(src_addr, src_port, dst_addr, dst_port)
+      assert IO.iodata_to_binary(header) == "PROXY TCP4 192.168.1.1 10.0.0.1 10000 443\r\n"
+    end
+  end
+
+  describe "format_header_v2/4" do
+    test "Handles IPv4" do
+      src_addr = {192, 168, 1, 1}
+      src_port = 10_000
+      dst_addr = {10, 0, 0, 1}
+      dst_port = 443
+      header = ProxyProtocol.format_header_v2(src_addr, src_port, dst_addr, dst_port)
+
+      # expected_header = [0x0D, 0x0A, 0x0D, 0x0A, 0x00, 0x0D, 0x0A, 0x51, 0x55, 0x49, 0x54, 0x0A, 0x02]
+      expected_header = [
+        13,
+        10,
+        13,
+        10,
+        0,
+        13,
+        10,
+        81,
+        85,
+        73,
+        84,
+        10,
+        "!",
+        <<17>>,
+        <<0, 12>>,
+        <<192, 168, 1, 1>>,
+        <<10, 0, 0, 1>>,
+        <<39, 16>>,
+        <<1, 187>>
+      ]
+
+      <<proxy_protocol_version::4, proxy_protocol_command::4>> = "!"
+      <<address_family::4, transport_protocol::4>> = <<17>>
+      assert proxy_protocol_version == 2
+      assert proxy_protocol_command == 1
+      assert address_family == 1
+      assert transport_protocol == 1
+      assert header == expected_header
+    end
   end
 end
